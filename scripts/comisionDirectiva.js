1198c23f7357fb983d04b8d56143118d5cab310d
emanuelperna <emanuelperna@icloud.com>
2025-11-11 01:28:46 -0300

    Add files via upload

---
+++ scripts/comisionDirectiva.js
@@ -0,0 +1,93 @@
+document.addEventListener('DOMContentLoaded', () => {
+      cargarComisionDirectiva();   // Muestra comision directiva
+});
+
+
+//Función para mostrar comision directiva
+const integrantesComision = [
+  {
+    nombre: "Cheetham Maximiliano",
+    cargo: "Presidente",
+  },
+  {
+    nombre: "Cuello Daniel",
+    cargo: "Vicepresidente",
+  },
+  {
+    nombre: "Pomba Vanesa",
+    cargo: "Secretaria",
+  },
+  {
+    nombre: "Gonzalez Camila",
+    cargo: "Pro-Secretaria",
+  },
+  {
+    nombre: "Cuello Adriana",
+    cargo: "Tesorero",
+  },
+  {
+    nombre: "Canavesio Analía",
+    cargo: "Pro-Tesorero",
+  },
+  {
+    nombre: "Massi Carlos",
+    cargo: "Primer Vocal",
+  },
+  {
+    nombre: "Dominguez Arlando",
+    cargo: "Segundo Vocal",
+  },
+  {
+    nombre: "Casali Bruno",
+    cargo: "Tercer Vocal",
+  },
+  {
+    nombre: "Gonzalez Roberto",
+    cargo: "Vocal Suplente",
+  },
+  {
+    nombre: "Anfossi Juan Pablo",
+    cargo: "Vocal Suplente",
+  },
+  {
+    nombre: "Molina Sergio",
+    cargo: "Vocal Suplente",
+  },
+  {
+    nombre: "Montiel Lino",
+    cargo: "Revisadores de Cuentas",
+  },
+  {
+    nombre: "Geuna Cristina",
+    cargo: "Revisadores de Cuentas",
+  },
+  {
+    nombre: "Molina Jorge",
+    cargo: "Revisadores de Cuentas",
+  },
+  {
+    nombre: "Actual Período",
+    cargo: "Gestion (2024 - 2026)",
+  },
+];
+
+// Función para renderizar los integrantes en el HTML
+function cargarComisionDirectiva() {
+  const container = document.getElementById("comision-directiva-container");
+
+  integrantesComision.forEach(integrante => {
+    const div = document.createElement("div");
+    div.className = "col-md-3 mb-4";
+
+    div.innerHTML = `
+      <div class="card h-100">
+        <div class="card-body">
+          <h5 class="card-title">${integrante.nombre}</h5>
+          <p class="card-text">${integrante.cargo}</p>
+        </div>
+      </div>
+    `;
+
+    container.appendChild(div);
+  });
+}
---
+++ scripts/scriptJugadores.js
@@ -0,0 +1,116 @@
+document.addEventListener('DOMContentLoaded', () => {
+      mostrarSecciones(); // Carga la lista de jugadores
+});
+
+    
+// Imágenes de cada jugador
+const imagenesJugadores = {
+  "Airasca Santiago": "assets/jugadores/Airasca-Santiago.jpeg",
+  "Gil Gabriel": "assets/jugadores/Gil-Gabriel.jpeg",
+  "Gonzalez Valentin": "assets/jugadores/Gonzalez-Valentin.jpeg",
+  "Andino Manuel": "assets/jugadores/Andino-Manuel.jpeg",
+  "Ferrando Marcos": "assets/jugadores/Ferrando-Marcos.jpeg",
+  "Fontana Santiago": "assets/jugadores/Fontana-Santiago.jpeg",
+  "Monzon Maximiliano": "assets/jugadores/Monzon-Maximiliano.jpeg",
+  "Velázquez Gaspar": "assets/jugadores/Velazquez-Gaspar.jpeg",
+  "Zabaleta Javier": "assets/jugadores/Zabaleta-Javier.jpeg",
+  "Rios Juan": "assets/jugadores/Rios-Juan.jpeg",
+  "Rodriguez Joaquín": "assets/jugadores/Rodriguez-Joaquin.jpeg",
+  "Montiel Gonzalo": "assets/jugadores/Montiel-Gonzalo.jpeg",
+  "Galetto Máximo": "assets/jugadores/Galetto-Maximo.jpeg",
+  "Guzmán Laureano": "assets/jugadores/Guzman-Laureano.jpeg",
+  "Arce Timoteo": "assets/jugadores/Arce-Timoteo.jpeg",
+  "Bocco Martin": "assets/jugadores/Bocco-Martin.jpeg",
+  "Cortez Fermin": "assets/jugadores/Cortez-Fermin.jpeg",
+  "Ferrando Joaquín": "assets/jugadores/Ferrando-Joaquin.jpeg",
+  "Granero Juan Pablo": "assets/jugadores/Granero-Juan-Pablo.jpeg",
+  "Reguero Franco": "assets/jugadores/Reguero-Franco.jpeg",
+  "Rodriguez Bernabe": "assets/jugadores/Rodriguez-Bernabe.jpeg",
+  "Moyano Francisco": "assets/jugadores/Moyano-Francisco.jpeg",
+  "Victorio Juan Ignacio": "assets/jugadores/Victorio-Juan-Ignacio.jpeg",
+  "Zarate Ezequiel": "assets/jugadores/Zarate-Ezequiel.jpeg",
+  "Dopazo Martin": "assets/jugadores/Dopazo-Martin.jpeg",
+  "Sanchez Bruno": "assets/jugadores/Sanchez-Bruno.jpeg",
+  "Cataldo Ramiro": "assets/jugadores/Cataldo-Ramiro.jpeg",
+  "Bruera Maximiliano": "assets/jugadores/Bruera-Maximiliano.jpeg",
+  "Alarcon Francisco": "assets/jugadores/Alarcon-Francisco.jpeg",
+  "Zabaleta Matias": "assets/jugadores/Zabaleta-Matias.jpeg",
+  
+};
+
+//Jugadores
+const jugadoresPorPosicion = [
+  {
+    posicion: "Arqueros",
+    nombres: ["Airasca Santiago", "Gil Gabriel", "Gonzalez Valentin"]
+  },
+  {
+    posicion: "Defensores",
+    nombres: [
+    "Andino Manuel", "Ferrando Marcos", "Fontana Santiago", 
+    "Monzon Maximiliano", "Velázquez Gaspar", "Zabaleta Javier",
+    "Rios Juan", "Rodriguez Joaquín", "Montiel Gonzalo",
+    "Galetto Máximo", "Guzmán Laureano"
+    ]
+  },
+  {
+    posicion: "Mediocampistas",
+    nombres: [
+    "Arce Timoteo", "Bocco Martin", "Cortez Fermin",
+    "Ferrando Joaquín", "Granero Juan Pablo", "Reguero Franco",
+    "Rodriguez Bernabe", "Moyano Francisco", "Victorio Juan Ignacio",
+    "Zarate Ezequiel"
+    ]
+  },
+  {
+    posicion: "Delanteros",
+    nombres: [
+    "Dopazo Martin", "Sanchez Bruno", "Cataldo Ramiro",
+    "Bruera Maximiliano", "Alarcon Francisco", "Zabaleta Matias"
+    ]
+  }
+];
+
+function mostrarSecciones() {
+  const seccionesContainer = document.getElementById('secciones-container');
+  const detalleContainer = document.getElementById('detalle-container');
+  seccionesContainer.innerHTML = '';
+  detalleContainer.innerHTML = '';
+
+  jugadoresPorPosicion.forEach((grupo, index) => {
+    const card = document.createElement('div');
+    card.className = 'col-md-3 mb-4';
+    card.innerHTML = `
+      <div class="card seccion-card h-100" onclick="mostrarDetalle(${index})">
+        <div class="card-body d-flex align-items-center justify-content-center">
+          <h4 class="card-title">${grupo.posicion}</h4>
+        </div>
+      </div>
+    `;
+    seccionesContainer.appendChild(card);
+  });
+}
+
+function mostrarDetalle(index) {
+  const grupo = jugadoresPorPosicion[index];
+  const detalleContainer = document.getElementById('detalle-container');
+  const seccionesContainer = document.getElementById('secciones-container');
+  seccionesContainer.innerHTML = '';
+
+  detalleContainer.innerHTML = `
+    <h3 class="titulo-posicion">${grupo.posicion}</h3>
+    <div class="row">
+      ${grupo.nombres.map(nombre => `
+        <div class="col-md-4 mb-4">
+          <div class="card jugador-card">
+            <div class="card-body">
+            <img src="${imagenesJugadores[nombre]}" alt="${nombre}" class="jugador-img mb-2">
+              <h5 class="card-title">${nombre}</h5>
+            </div>
+          </div>
+        </div>
+      `).join('')}
+    </div>
+    <button class="btn btn-outline-danger mt-3" onclick="mostrarSecciones()">Volver</button>
+  `;
+}
